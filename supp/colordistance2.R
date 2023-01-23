fb3b62d03dbe63ceec3ad487ae3885f467a7cfca
aksel-blaise <zac_selden@pm.me>
2023-01-23 10:20:21 -0600

    <edit index>

--- supp/colordistance2.R
+++ supp/colordistance2.R
@@ -1,36 +1,30 @@
----
-title: "Raw Material Color Groups"
----
-
-```{r color, out.width = "100%", dpi = 300, echo=TRUE, warning=FALSE, message=FALSE}
 # compare colors
 library(here)
 library(colordistance)
 library(recolorize)
-library(vegan)
-library(ape)
 
 # get image paths
+# convert from rgb to CIELab
 images <- colordistance::getImagePaths('la.gahagan')
 
 # generate color distance matrix
 cdm <- imageClusterPipeline(images)
-```
 
-Plot distance matrix as bivariate plot using non-metric multidimensional scaling.
+library(ape)
+tree <- nj(as.dist(cdm))
+plot(tree, direction = "upwards", cex = 0.5)
 
-```{r colorspaceplot, out.width = "100%", dpi = 300, echo=TRUE, warning=FALSE, message=FALSE}
 # define add_image function:
-add_image <- function(obj,
-                      x = NULL,
+add_image <- function(obj, # an object interpretable by rasterImage
+                      x = NULL, # x & y coordinates for the center of the image
                       y = NULL,
-                      width = NULL,
-                      interpolate = TRUE,
+                      width = NULL, # width of the image
+                      interpolate = TRUE, # method for resizing
                       angle = 0) {
   
   # get current plotting window parameters:
-  usr <- graphics::par()$usr
-  pin <- graphics::par()$pin
+  usr <- graphics::par()$usr # extremes of user coordinates in the plotting region
+  pin <- graphics::par()$pin # plot dimensions (in inches)
   
   # image dimensions and scaling factor:
   imdim <- dim(obj)
@@ -38,8 +32,8 @@ add_image <- function(obj,
   
   # set the width of the image (relative to x-axis)
   w <- width / (usr[2] - usr[1]) * pin[1]
-  h <- w * sf
-  hu <- h / pin[2] * (usr[4] - usr[3])
+  h <- w * sf # height is proportional to width
+  hu <- h / pin[2] * (usr[4] - usr[3]) # scale height to y-axis range
   
   # plot the image
   graphics::rasterImage(image = obj,
@@ -49,52 +43,40 @@ add_image <- function(obj,
                         angle = angle)
 }
 
-# colorspace + images
+# explaining NMDS is beyond the scope of this post (and is probably best left to the ecologists)
+# see this link for more: 
+# https://cougrstats.wordpress.com/2019/12/11/non-metric-multidimensional-scaling-nmds-in-r/
+
+# for now, we'll just do it in two lines!
+library(vegan)
 nmds_scores <- scores(metaMDS(comm = as.dist(cdm)))
 
-# set plot parameters
-plot(nmds_scores,
-     xlim = c(-0.45, 0.4),
-     ylim = c(-0.1, 0.13),
-     cex = 0.001
-     )
-
-# add images
+plot(nmds_scores)
 for (i in 1:length(images)) {
   
-  # read image:
+  # read the image into R:
   img <- png::readPNG(images[i]) 
   
-  # add image:
-  add_image(img, 
-            x = nmds_scores[i, 1], 
-            y = nmds_scores[i, 2],
+  # add the image:
+  add_image(img, x = nmds_scores[i, 1], y = nmds_scores[i, 2],
             width = 0.025)
 }
-```
 
-Plot images at tips of the hierarchical clustering tree shown on the top/left of heatmap.
+# plot the tree
+plot(tree, show.tip.label = FALSE, direction = "upwards")
 
-```{r tree, out.width = "100%", dpi = 300, echo=TRUE, warning=FALSE, message=FALSE}
-# neighbor-joining tree
-tree <- nj(as.dist(cdm))
-# plot tree
-plot(tree,
-     show.tip.label = FALSE,
-     direction = "upward")
+# get the parameters from the plotting environment
+lastPP <- get("last_plot.phylo", envir = .PlotPhyloEnv)
 
-# get parameters from plotting environment
-lastPP <- get("last_plot.phylo", 
-              envir = .PlotPhyloEnv)
-
-# get xy coordinates of tips
+# get the xy coordinates of the tips
 ntip <- lastPP$Ntip
 
-# first n values are tips/remaining values are node coordinates
+# first n values are the tips, remaining values are the coordinates of the
+# nodes:
 xy <- data.frame(x = lastPP$xx[1:ntip],
                  y = lastPP$yy[1:ntip]) 
 
-# add points
+# we can add points to the tree pretty easily using generic functions:
 points(xy[ , 1], xy[ , 2], 
        col = viridisLite::viridis(40), 
        pch = 19, cex = 2)
@@ -105,18 +87,19 @@ imnames <- tools::file_path_sans_ext(basename(images))
 # get tip labels
 tipnames <- tree$tip.label
 
-# check that images are in correct order:
+# in my case, the tip labels are identical to the image names, so I can
+# use these to check that my images are in the right order:
 image_order <- match(tipnames, imnames)
 images <- images[image_order]
 
-## final plot
+# and plot!
 par(mar = rep(0, 4))
 plot(tree, show.tip.label = FALSE, direction = "upward")
 
-# parameters from plotting environment
+# get the parameters from the plotting environment
 lastPP <- get("last_plot.phylo", envir = .PlotPhyloEnv)
 
-# xy coordinates of tips
+# get the xy coordinates of the tips
 ntip <- lastPP$Ntip
 xy <- data.frame(x = lastPP$xx[1:ntip],
                  y = lastPP$yy[1:ntip]) 
@@ -125,6 +108,5 @@ for (i in 1:length(images)) {
   add_image(png::readPNG(images[i]),
             x = xy[i, 1],
             y = xy[i, 2], 
-            width = 0.75)
+            width = 0.5)
 }
-```
